c1155afd4c26840e1ea695528a4bfe1c2dd686cf
bayutria11 <74871436+bayutria11@users.noreply.github.com>
2022-10-21 16:36:18 +0700

    Add files via upload

---
+++ kentanktech-production/application/views/user/AboutView.php
@@ -0,0 +1,93 @@
+<img src="<?php echo base_url(); ?>assets/images/gb2.png" class="img-fluid" alt="Responsive image" width="2000">
+
+<div class="block-quick-info-2">
+    <div class="container">
+        <div class="block-quick-info-2-inner">
+            <div class="row">
+                <div class="col-sm-6 col-md-6 col-lg-3 mb-3 mb-lg-0">
+                    <div class="d-flex quick-info-2">
+                        <span class="icon icon-home mr-3"></span>
+                        <div class="text">
+                            <strong class="d-block heading">Visit our Location</strong>
+                            <span class="excerpt">Somewhere in Indonesia</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-3 mb-3 mb-lg-0">
+                    <div class="d-flex quick-info-2">
+                        <span class="icon icon-phone mr-3"></span>
+                        <div class="text">
+                            <strong class="d-block heading">Call us today</strong>
+                            <span class="excerpt"><a href="#">+62 12345678</a></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-3 mb-3 mb-lg-0">
+                    <div class="d-flex quick-info-2">
+                        <span class="icon icon-envelope mr-3"></span>
+                        <div class="text">
+                            <strong class="d-block heading">Send us a message</strong>
+                            <span class="excerpt"><a href="#">kentanktech@gmail.com</a></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-3 mb-3 mb-lg-0">
+                    <div class="d-flex quick-info-2">
+                        <span class="icon icon-clock-o mr-3"></span>
+                        <div class="text">
+                            <strong class="d-block heading">Opening hours</strong>
+                            <span class="excerpt">Mon - Sat 8:00 AM - 9:00 PM</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="site-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="<?php echo base_url(); ?>assets/images/lead2.jpeg" alt="Image" class="img-fluid rounded mb-3">
+                <img src="<?php echo base_url(); ?>assets/images/lead.jpg" alt="Image" class="img-fluid rounded">
+            </div>
+            <div class="col-md-5 ml-auto">
+                <h2 class="text-primary mb-3">About Our Website</h2>
+                <p class="lead">Brand New Corporate Technology, Give you new insight, Allow you to upgrade</p>
+
+                <p class="mb-4"> gives you technology options for your computer. We provide a variety of reliable suppliers so that you can specifically improve the performance of your computer.</p>
+
+                <p class="mb-4"> we are very dedicated to service and your satisfaction in using our services. There are thousands of products that we offer that are expected to develop your abilities.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="site-section">
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-7 text-center">
+                <h2 class="text-primary mb-3">Our Team</h2>
+                <p></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 col-lg-6 mb-4 mb-lg-5 text-center">
+                <img src="<?php echo base_url(); ?>assets/images/fiky.jpg" alt="Image" class="img-fluid mb-3 w-50 rounded-circle">
+                <h3 class="h4">Fiky Anggara</h3>
+                <p class="caption text-primary">Student</p>
+                <p> The first member and the leader of the group <br>
+                    Bis zur Unendlichkeit und weiter</p>
+            </div>
+            <div class="col-md-6 col-lg-6 mb-4 mb-lg-5 text-center">
+                <img src="<?php echo base_url(); ?>assets/images/bayu.jpg" alt="Image" class="img-fluid mb-3 w-50 rounded-circle">
+                <h3 class="h4">Bayu Tri Aditia</h3>
+                <p class="caption text-primary">Student</p>
+                <p>The second member <br> Bernafas </p>
+            </div>
+
+        </div>
+    </div>
+</div>
+
---
+++ kentanktech-production/application/views/user/HomeView.php
@@ -0,0 +1,59 @@
+<img src="<?php echo base_url(); ?>assets/images/tech.jpg" class="img-fluid" width="2000" alt="Responsive image" style="display: block; margin: auto;">
+
+<div class="container">
+    <div class="row align-items-center justify-content-center text-center">
+
+        <div class="col-md-10">
+
+            <div class="row justify-content-center mb-4">
+                <div class="col-md-10 text-center">
+                    <h1 data-aos="fade-up" class="mb-5"> Modern Problems Need Modern Solutions <span class="typed-words"></span></h1>
+                    <p data-aos="fade-up" data-aos-delay="100"><a href="<?php echo base_url('index.php/home/supplier') ?>" class="btn btn-primary btn-pill">Find a Supplier</a></p>
+                    <p data-aos="fade-up" data-aos-delay="100"><a href="<?php echo base_url('index.php/home/products') ?>" class="btn btn-primary btn-pill">Find a Product</a></p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="block-services-1 py-5">
+    <div class="container">
+        <div class="row">
+            <div class="mb-4 mb-lg-0 col-sm-6 col-md-6 col-lg-3">
+                <h3 class="mb-3">Get the latest products!</h3>
+                <p>We offer a vast number of medical informations around the globe to keep you updated. Remember to wash your hands before and after traveling. Stay safe. </p>
+            </div>
+            <div class="mb-4 mb-lg-0 col-sm-6 col-md-6 col-lg-3">
+                <div class="block-service-1-card">
+                    <a href="#" class="thumbnail-link d-block mb-4"><img src="<?php echo base_url(); ?>assets/images/RTX3080.jpg" alt="Image" class="img-fluid"></a>
+                    <h3 class="block-service-1-heading mb-3"><a href="#">RTX 3080</a></h3>
+                    <div class="block-service-1-excerpt">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper velit sed ullamcorper morbi. </p>
+                    </div>
+                    <p><a href="<?php echo base_url('index.php/home/products') ?>" class="d-inline-flex align-items-center block-service-1-more"><span>Find out more</span> <span class="icon-keyboard_arrow_right icon"></span></a></p>
+                </div>
+            </div>
+            <div class="mb-4 mb-lg-0 col-sm-6 col-md-6 col-lg-3">
+                <div class="block-service-1-card">
+                    <a href="#" class="thumbnail-link d-block mb-4"><img src="<?php echo base_url(); ?>assets/images/zephyrus.jpg" alt="Image" class="img-fluid"></a>
+                    <h3 class="block-service-1-heading mb-3"><a href="#">ASUS ZEPHYRUS</a></h3>
+                    <div class="block-service-1-excerpt">
+                        <p>Proin fermentum leo vel orci porta. Lobortis feugiat vivamus at augue eget. Aliquam nulla facilisi cras fermentum odio eu feugiat. Arcu bibendum at varius vel.</p>
+                    </div>
+                    <p><a href="<?php echo base_url('index.php/home/products') ?>" class="d-inline-flex align-items-center block-service-1-more"><span>Find out more</span> <span class="icon-keyboard_arrow_right icon"></span></a></p>
+                </div>
+            </div>
+            <div class="mb-4 mb-lg-0 col-sm-6 col-md-6 col-lg-3">
+                <div class="block-service-1-card">
+                    <a href="#" class="thumbnail-link d-block mb-4"><img src="<?php echo base_url(); ?>assets/images/keyboard.jpg" alt="Image" class="img-fluid"></a>
+                    <h3 class="block-service-1-heading mb-3"><a href="#">MECHANICAL KEYBOARD</a></h3>
+                    <div class="block-service-1-excerpt">
+                        <p>Adipiscing enim eu turpis egestas pretium aenean pharetra magna ac. Amet cursus sit amet dictum. Nullam non nisi est sit amet facilisis magna.</p>
+                    </div>
+                    <p><a href="<?php echo base_url('index.php/home/products') ?>" class="d-inline-flex align-items-center block-service-1-more"><span>Find out more</span> <span class="icon-keyboard_arrow_right icon"></span></a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
